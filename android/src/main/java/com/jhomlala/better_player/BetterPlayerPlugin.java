5860681ecb875a6ee1a26dbc78c70c2604a4fa6c
Vis5 <themonmail@gmail.com>
2021-07-24 18:03:45 +0800

    unstable to stable

--- android/src/main/java/com/jhomlala/better_player/BetterPlayerPlugin.java
+++ android/src/main/java/com/jhomlala/better_player/BetterPlayerPlugin.java
@@ -493,7 +493,13 @@ public class BetterPlayerPlugin implements FlutterPlugin, ActivityAware, MethodC
 
     private void enablePictureInPicture(BetterPlayer player) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // player.setupMediaSession(flutterState.applicationContext, true);
+            // Original code
+            /*player.setupMediaSession(flutterState.applicationContext, true);
+            activity.enterPictureInPictureMode(new PictureInPictureParams.Builder().build());
+            startPictureInPictureListenerTimer(player);
+            player.onPictureInPictureStatusChanged(true);*/
+            
+            player.setupMediaSession(flutterState.applicationContext, true);
             pipPaused = true;
             updatePipButton(player, 0, true);
 
